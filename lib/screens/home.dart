a084287884aca2bd3bc42403df3589550531e44f
pacmadhushan <chathuramadhushanpa@gmail.com>
2023-04-03 23:52:38 +0530

    add bar set

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -16,31 +16,94 @@ class Home extends StatelessWidget {
       backgroundColor: Color(0xFFDDECF6),
       appBar: _buildAppBar(),
       // ignore: avoid_unnecessary_containers
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-        child: Column(children: [
-          searchBox(),
-          Expanded(
-            child: ListView(
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Column(children: [
+              searchBox(),
+              Expanded(
+                child: ListView(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(
+                        top: 50,
+                        bottom: 20,
+                      ),
+                      child: const Text(
+                        'All ToDos',
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    for (ToDo toDoo in todosList)
+                      ToDoItem(
+                        todo: toDoo,
+                      ),
+                  ],
+                ),
+              )
+            ]),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
               children: [
-                Container(
-                  margin: const EdgeInsets.only(
-                    top: 50,
-                    bottom: 20,
-                  ),
-                  child: const Text(
-                    'All ToDos',
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                      bottom: 20,
+                      right: 20,
+                      left: 20,
+                    ),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      // ignore: prefer_const_literals_to_create_immutables
+                      boxShadow: [
+                        const BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0.0, 0.0),
+                          blurRadius: 10.0,
+                          spreadRadius: 0.0,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                          hintText: 'Add a new todo item',
+                          border: InputBorder.none),
+                    ),
                   ),
                 ),
-                for (ToDo toDoo in todosList)
-                  ToDoItem(
-                    todo: toDoo,
+                // ignore: prefer_const_constructors
+                Container(
+                  margin: const EdgeInsets.only(
+                    bottom: 20,
+                    right: 20,
                   ),
+                  child: ElevatedButton(
+                    // ignore: sort_child_properties_last
+                    child: const Text(
+                      '+',
+                      style: TextStyle(
+                        fontSize: 40,
+                      ),
+                    ),
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      minimumSize: const Size(60, 60),
+                      elevation: 10,
+                    ),
+                  ),
+                )
               ],
             ),
           )
-        ]),
+        ],
       ),
     );
   }
